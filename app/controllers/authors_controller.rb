5e0e4e448a63cbdbcbce8199432842244b9662da
dwiheru <dwi.heru@mekari.com>
2024-09-02 22:10:28 +0700

    changed: adding comment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,6 +5,29 @@ class AuthorsController < ApplicationController
   before_action :validate_form, only: %i[create update]
 
   # GET /authors
+  # Returns list of authors with pagination information
+  #
+  # @example Response
+  #   {
+  #     "authors": [
+  #       {
+  #         "id": "uuid",
+  #         "name": "string",
+  #         "bio": "string",
+  #         "birthdate": "date",
+  #         "created_at": "datetime",
+  #         "updated_at": "datetime"
+  #       }
+  #     ],
+  #     "paginate": {
+  #       "current_page": number,
+  #       "per_page": number,
+  #       "total_entries": number,
+  #       "total_pages": number
+  #     }
+  #   }
+  #
+  # @return [JSON] response with list of authors and pagination information
   def index
     @authors = Author.includes(:books).page(page).per(per_page)
 
@@ -12,6 +35,21 @@ class AuthorsController < ApplicationController
   end
 
   # GET /authors/:id
+  #
+  # Returns a specific author
+  #
+  # @example Response
+  #   {
+  #     "id": "uuid",
+  #     "name": "string",
+  #     "bio": "string",
+  #     "birthdate": "date",
+  #     "created_at": "datetime",
+  #     "updated_at": "datetime"
+  #   }
+  #
+  # @param id [String] uuid of the author
+  # @return [JSON] response with the author
   def show
     Rails.cache.fetch("author:#{@author.id}", expires_in: 1.hours) do
       json_response(@author, :ok)
@@ -19,6 +57,28 @@ class AuthorsController < ApplicationController
   end
 
   # POST /authors
+  #
+  # Creates a new author
+  #
+  # @example Request body
+  #   {
+  #     "name": "string",
+  #     "bio": "string",
+  #     "birthdate": "date"
+  #   }
+  #
+  # @example Response
+  #   {
+  #     "id": "uuid",
+  #     "name": "string",
+  #     "bio": "string",
+  #     "birthdate": "date",
+  #     "created_at": "datetime",
+  #     "updated_at": "datetime"
+  #   }
+  #
+  # @param [Hash] author_params The parameters for the new author
+  # @return [JSON] response with the created author
   def create
     @author = Author.new(author_params)
     @author.save
@@ -29,6 +89,28 @@ class AuthorsController < ApplicationController
   end
 
   # PATCH/PUT /authors/:id
+  #
+  # Updates an existing author
+  #
+  # @example Request body
+  #   {
+  #     "name": "string",
+  #     "bio": "string",
+  #     "birthdate": "date"
+  #   }
+  #
+  # @example Response
+  #   {
+  #     "id": "uuid",
+  #     "name": "string",
+  #     "bio": "string",
+  #     "birthdate": "date",
+  #     "created_at": "datetime",
+  #     "updated_at": "datetime"
+  #   }
+  #
+  # @param [Hash] author_params The parameters for the updated author
+  # @return [JSON] response with the updated author
   def update
     @author.update(author_params)
 
@@ -38,6 +120,10 @@ class AuthorsController < ApplicationController
   end
 
   # DELETE /authors/:id
+  #
+  # Deletes an existing author
+  #
+  # @return [JSON] response with empty body
   def destroy
     @author.destroy
 
@@ -45,6 +131,33 @@ class AuthorsController < ApplicationController
   end
 
   # GET /authors/:id/books
+  #
+  # Returns a list of books that are associated to the author
+  #
+  # @example Request
+  #   GET /authors/:id/books?page=1&per_page=10
+  #
+  # @example Response
+  #   {
+  #     "books": [
+  #       {
+  #         "id": "uuid",
+  #         "title": "string",
+  #         "description": "string",
+  #         "publish_date": "date"
+  #       }
+  #     ],
+  #     "paginate": {
+  #       "current_page": number,
+  #       "per_page": number,
+  #       "total_entries": number,
+  #       "total_pages": number
+  #     }
+  #   }
+  #
+  # @param [Hash] params The parameters that are passed to the action
+  # @return [JSON] response with a list of books that are associated to the author
+  #   and pagination information
   def books
     @books = @author.books.page(page).per(per_page)
 
@@ -53,22 +166,41 @@ class AuthorsController < ApplicationController
 
   private
 
+  # Finds the author by id param
+  #
+  # @return [Author] the requested author
+  # @raise [JSON] with status :not_found if the author is not found
   def set_author
     @author = Author.find_by_id(params[:id])
 
     json_response({ message: 'Author not found' }, :not_found) and return unless @author.present?
   end
 
+  # @return [Hash] params for the author
+  #
+  # @api private
   def author_params
     @author_params ||= params.permit(:name, :bio, :birthdate)
   end
 
+  # Validates the form params
+  #
+  # @return [JSON] response with empty body or
+  #   with status :unprocessable_entity if the form is invalid
   def validate_form
+    # Validates the form params
     validate = Authors::Form.new(author_params)
 
+    # If the form is invalid, returns the errors
+    # in a JSON response with status :unprocessable_entity
     json_response(validate.errors, :unprocessable_entity) and return unless validate.valid?
   end
 
+  # Returns the list of books in a format
+  # that is suitable to be converted to JSON
+  #
+  # @return [Array<Hash>] the list of books in a format
+  #   that is suitable to be converted to JSON
   def response_book_list
     @books.map do |book|
       {
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,30 @@ class BooksController < ApplicationController
   before_action :validate_form, only: %i[create update]
 
   # GET /books
+  # Returns list of books with pagination
+  #
+  # @example
+  #   GET /books
+  #   {
+  #     "books": [
+  #       {
+  #         "id": 1,
+  #         "title": "My Book",
+  #         "description": "This is my book",
+  #         "publish_date": "1994-01-01",
+  #         "author": {
+  #           "id": 1,
+  #           "name": "John Doe"
+  #         }
+  #       }
+  #     ],
+  #     "paginate": {
+  #       "current_page": 1,
+  #       "per_page": 10,
+  #       "total_entries": 100,
+  #       "total_pages": 10
+  #     }
+  #   }
   def index
     @books = Book.includes(:author).page(page).per(per_page)
 
@@ -12,6 +36,23 @@ class BooksController < ApplicationController
   end
 
   # GET /books/:id
+  #
+  # Returns a single book by :id
+  #
+  # @example
+  #   GET /books/1
+  #   {
+  #     "id": 1,
+  #     "title": "My Book",
+  #     "description": "This is my book",
+  #     "publish_date": "1994-01-01",
+  #     "author": {
+  #       "id": 1,
+  #       "name": "John Doe"
+  #     }
+  #   }
+  #
+  # The response will be cached for 1 hour
   def show
     Rails.cache.fetch("book:#{@book.id}", expires_in: 1.hours) do
       json_response(@book, :ok)
@@ -19,6 +60,21 @@ class BooksController < ApplicationController
   end
 
   # POST /books
+  #
+  # Creates a new book with the given parameters
+  #
+  # @example
+  #   POST /books
+  #   {
+  #     "title": "My Book",
+  #     "description": "This is my book",
+  #     "publish_date": "1994-01-01",
+  #     "author_id": 1
+  #   }
+  #
+  # The response will be cached for 1 hour
+  #
+  # @param [Hash] book_params The parameters to create the book
   def create
     @book = Book.new(book_params)
     @book.save
@@ -29,6 +85,21 @@ class BooksController < ApplicationController
   end
 
   # PATCH/PUT /books/:id
+  #
+  # Updates the book with the given parameters
+  #
+  # @example
+  #   PATCH /books/1
+  #   {
+  #     "title": "My Book",
+  #     "description": "This is my book",
+  #     "publish_date": "1994-01-01",
+  #     "author_id": 1
+  #   }
+  #
+  # The response will be cached for 1 hour
+  #
+  # @param [Hash] book_params The parameters to update the book
   def update
     @book.update(book_params)
 
@@ -38,30 +109,69 @@ class BooksController < ApplicationController
   end
 
   # DELETE /books/:id
+  #
+  # Deletes the book with the given :id
+  #
+  # @example
+  #   DELETE /books/1
+  #
+  # The response will be cached for 1 hour
   def destroy
+    # Destroy the book
     @book.destroy
 
+    # Return an empty JSON response with HTTP status :ok
     json_response({}, :ok)
   end
 
   private
 
+  # Finds the book with the given :id
+  #
+  # @example
+  #   GET /books/1
+  #
+  # The response will be cached for 1 hour
   def set_book
+    # Find the book with the given :id
     @book = Book.find_by_id(params[:id])
 
+    # If the book is not found, return an empty JSON response with HTTP status :not_found
     json_response({ message: 'Book not found' }, :not_found) and return unless @book.present?
   end
 
+  # The parameters that are permitted for the book
+  #
+  # @return [ActionController::Parameters] The permitted parameters
   def book_params
     @book_params ||= params.permit(:title, :description, :publish_date, :author_id)
   end
 
+  # Validates the book form
+  #
+  # @example
+  #   POST /books
+  #   {
+  #     "book": {
+  #       "title": "The Great Gatsby",
+  #       "description": "The Great Gatsby is a novel by F. Scott Fitzgerald",
+  #       "publish_date": "1925-04-10",
+  #       "author_id": 1
+  #     }
+  #   }
+  #
+  # The response will be cached for 1 hour
   def validate_form
+    # Create a Books::Form object
     validate = Books::Form.new(book_params)
 
+    # If the form is not valid, return an empty JSON response with HTTP status :unprocessable_entity
     json_response(validate.errors, :unprocessable_entity) and return unless validate.valid?
   end
 
+  # The list of books with pagination
+  #
+  # @return [Array<Hash>] The list of books with pagination
   def response_book_list
     @books.map do |book|
       {
